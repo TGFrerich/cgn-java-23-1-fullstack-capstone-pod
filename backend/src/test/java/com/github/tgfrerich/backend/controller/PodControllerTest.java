6414a6d38acef646f313a3b87cee34bc31176c63
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 14:53:02 +0100

    created positive test for sendUrl Api

--- backend/src/test/java/com/github/tgfrerich/backend/controller/PodControllerTest.java
+++ backend/src/test/java/com/github/tgfrerich/backend/controller/PodControllerTest.java
@@ -1,10 +1,15 @@
 package com.github.tgfrerich.backend.controller;
 
-import com.github.tgfrerich.backend.repository.PodRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,12 +17,23 @@ class PodControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    
 
-    @Autowired
-    private PodRepository podRepository;
+    String testUrl = "https://open.spotify.com/episode/34fULWIqFeFmM7yBdo2ltg?si=dbffc903c55f4b61";
 
-
+    @Test
+    @DirtiesContext
     void WhenSuccessfulPostUrlGetUrlBack() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/podcasts")
+                        .contentType("text/plain;charset=UTF-8")
+                        .content(testUrl))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String actualResponse = result.getResponse().getContentAsString();
+
+        Assertions.assertEquals(testUrl, actualResponse);
 
 
     }
