80df9c1c3645c47529ae1cbe0b4a6dad64b2e992
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 14:18:34 +0100

    changed Post Endpoint to send url as body not as pathvariable

--- backend/src/main/java/com/github/tgfrerich/backend/controller/PodController.java
+++ backend/src/main/java/com/github/tgfrerich/backend/controller/PodController.java
@@ -1,8 +1,8 @@
 package com.github.tgfrerich.backend.controller;
 
 import com.github.tgfrerich.backend.service.PodService;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +15,8 @@ public class PodController {
         this.podService = podService;
     }
 
-    @PostMapping("/{id}")
-    public String sendUrl(@PathVariable String url) {
+    @PostMapping("/podcasts")
+    public String sendUrl(@RequestBody String url) {
         return podService.sendUrl(url);
     }
 }
