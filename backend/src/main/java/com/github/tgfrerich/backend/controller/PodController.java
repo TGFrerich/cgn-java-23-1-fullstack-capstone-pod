44b9d741601121f2bffa155611017f06e8fd2494
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 12:01:40 +0100

    created REST Post Endpoint in PodController

--- backend/src/main/java/com/github/tgfrerich/backend/controller/PodController.java
+++ backend/src/main/java/com/github/tgfrerich/backend/controller/PodController.java
@@ -1,4 +1,22 @@
 package com.github.tgfrerich.backend.controller;
 
+import com.github.tgfrerich.backend.service.PodService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class PodController {
+    private final PodService podService;
+
+    public PodController(PodService podService) {
+        this.podService = podService;
+    }
+
+    @PostMapping("/{id}")
+    public String sendUrl(@PathVariable String url) {
+        return podService.sendUrl(url);
+    }
 }
