8fe75c3c81ce29d9788c9c0eeef7e7310f5c2a8f
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 11:20:59 +0100

    started first controller test for TDD

--- backend/src/test/java/com/github/tgfrerich/backend/controller/PodControllerTest.java
+++ backend/src/test/java/com/github/tgfrerich/backend/controller/PodControllerTest.java
@@ -1,5 +1,24 @@
 package com.github.tgfrerich.backend.controller;
 
+import com.github.tgfrerich.backend.repository.PodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class PodControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private PodRepository podRepository;
+
+
+    void WhenSuccessfulPostUrlGetUrlBack() throws Exception {
+
+
+    }
 }
