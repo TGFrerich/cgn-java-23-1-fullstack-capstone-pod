26975b0ce77ecb1499d12fcae6f301c8d1d96132
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 12:08:15 +0100

    created sendUrl Method in PodService

--- backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
+++ backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
@@ -1,4 +1,8 @@
 package com.github.tgfrerich.backend.service;
 
 public class PodService {
+    public String sendUrl(String url) {
+        String inTheFutureUrlWillBeCheckedIfAlreadyInDatabase = url;
+        return inTheFutureUrlWillBeCheckedIfAlreadyInDatabase;
+    }
 }
