af1bc58efa5ca96746500ebc5cbae9aeb5a045c6
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 14:17:26 +0100

    changed PodService.sendUrl to receive body instead pathvariable

--- backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
+++ backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
@@ -1,6 +1,17 @@
 package com.github.tgfrerich.backend.service;
+import com.github.tgfrerich.backend.repository.PodRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PodService {
+    private final IdService idService;
+    private final PodRepository podRepository;
+
+    public PodService(PodRepository podRepository, IdService idService) {
+        this.podRepository = podRepository;
+        this.idService = idService;
+    }
+
     public String sendUrl(String url) {
         String inTheFutureUrlWillBeCheckedIfAlreadyInDatabase = url;
         return inTheFutureUrlWillBeCheckedIfAlreadyInDatabase;
