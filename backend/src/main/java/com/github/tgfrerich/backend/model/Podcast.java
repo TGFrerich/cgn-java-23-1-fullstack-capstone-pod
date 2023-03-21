2a5286effcf130b4f9f0bc6d52bc383289aa585c
TG Frerich <themuppet.coder@gmail.com>
2023-03-21 11:28:24 +0100

    created attributes for Podcast

--- backend/src/main/java/com/github/tgfrerich/backend/model/Podcast.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/Podcast.java
@@ -1,4 +1,29 @@
 package com.github.tgfrerich.backend.model;
 
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document("podcasts")
 public class Podcast {
+
+    private String acoustic_model;
+    private double audio_duration;
+    private String audio_url;
+    private double confidence;
+    private Boolean dual_channel;
+    private Boolean format_text;
+    private String id;
+    private String language_model;
+    private Boolean punctuate;
+    private String status;
+    private String text;
+    private List<Word> words;
+    private Object utterances;
+    private Integer webhook_status_code;
+    private String webhook_url;
+
+
 }
