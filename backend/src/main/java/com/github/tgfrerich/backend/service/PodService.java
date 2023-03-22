5534f8082f237c0bd01a4a89457d6d2b4e7ff1ba
TG Frerich <themuppet.coder@gmail.com>
2023-03-22 11:43:26 +0100

    extended the sendUrl method to throw Exception if Url is empty

--- backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
+++ backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
@@ -1,6 +1,8 @@
 package com.github.tgfrerich.backend.service;
 import com.github.tgfrerich.backend.repository.PodRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class PodService {
@@ -13,7 +15,12 @@ public class PodService {
     }
 
     public String sendUrl(String url) {
-        String inTheFutureUrlWillBeCheckedIfAlreadyInDatabase = url;
-        return inTheFutureUrlWillBeCheckedIfAlreadyInDatabase;
+        if (url.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "URL Required");
+        } else {
+            String inTheFutureUrlWillBeCheckedIfAlreadyInDatabase = url;
+            return inTheFutureUrlWillBeCheckedIfAlreadyInDatabase;
+        }
+
     }
 }
