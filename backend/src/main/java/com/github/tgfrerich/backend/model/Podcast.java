fc7b99f97880348400cf6548e5cbe918f7462040
TG Frerich <themuppet.coder@gmail.com>
2023-03-22 10:42:02 +0100

    added constructors to Podcast model

--- backend/src/main/java/com/github/tgfrerich/backend/model/Podcast.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/Podcast.java
@@ -1,9 +1,13 @@
 package com.github.tgfrerich.backend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document("podcasts")
 public class Podcast {
 
