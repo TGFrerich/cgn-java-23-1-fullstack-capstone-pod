cb4c846513d76181bc2d8c5a694b7823a7c320d1
TG Frerich <themuppet.coder@gmail.com>
2023-03-24 16:21:39 +0100

    created model for Assembly AI's Api response

---
+++ backend/src/main/java/com/github/tgfrerich/backend/model/AssemblyAIApiResponse.java
@@ -0,0 +1,15 @@
+package com.github.tgfrerich.backend.model;
+
+public class AssemblyAIApiResponse {
+
+    String id;
+    String status;
+    String acoustic_model;
+    int audio_duration;
+    String audio_url;
+    Boolean format_text;
+    String language_model;
+    String punctuate;
+    String text;
+
+}
