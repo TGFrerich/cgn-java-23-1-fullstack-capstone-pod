5bade151a58ee17e909e0c1b43902d86aaf70036
TG Frerich <themuppet.coder@gmail.com>
2023-03-27 12:44:10 +0200

    created Service for Assembly AI Api call

--- backend/src/main/java/com/github/tgfrerich/backend/service/AssemblyAIApiService.java
+++ backend/src/main/java/com/github/tgfrerich/backend/service/AssemblyAIApiService.java
@@ -1,35 +1,29 @@
 package com.github.tgfrerich.backend.service;
 
-import com.github.tgfrerich.backend.model.AssemblyAIApiResponse;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Service
-
 public class AssemblyAIApiService {
 
     private static final String ASSEMBLY_AI_API_ENDPOINT = "https://api.assemblyai.com/v2";
     private static final String AUTH_TOKEN_ASSEMBLYAI = System.getenv("API_KEY_ASSEMBLYAI");
 
-    private static WebClient webClient;
+    private WebClient webClient = WebClient.create(ASSEMBLY_AI_API_ENDPOINT);
 
-    public void AssemblyAIClient(String apiKey) {
-        this.webClient = WebClient.builder()
-                .baseUrl(ASSEMBLY_AI_API_ENDPOINT)
-                .defaultHeader("Authorization", AUTH_TOKEN_ASSEMBLYAI)
-                .build();
-    }
 
-    public Mono<AssemblyAIApiResponse> transcribeAudio(String audioUrl) {
-        return this.webClient.post()
+    public String transcribeAudio(String audioUrl) {
+        //"{\"audio_url\": \"" + audioUrl + "\"}"
+        var body = "{\n" +
+                "\"audio_url\":\n" + audioUrl +
+                "}";
+        return webClient.post()
                 .uri("/transcript")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue("{\"audio_url\": \"" + audioUrl + "\"}"))
+                .header("Authorization", AUTH_TOKEN_ASSEMBLYAI)
+                .bodyValue(audioUrl)
                 .retrieve()
-                .bodyToMono(AssemblyAIApiResponse.class);
+                .bodyToMono(String.class)
+                .block();
     }
 }
 
