c535e04276d6bd8ad130430c1fcf0753d5a1288f
TG Frerich <themuppet.coder@gmail.com>
2023-03-29 12:14:47 +0200

    updated RequestBodyForAssemblyAI.java to only consist of "audio_url" attribute

--- backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
@@ -1,5 +1,10 @@
 package com.github.tgfrerich.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class RequestBodyForAssemblyAI {
 
     String audio_url;
