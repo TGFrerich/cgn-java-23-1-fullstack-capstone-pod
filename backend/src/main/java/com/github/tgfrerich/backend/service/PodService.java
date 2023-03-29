d77d0468f42c7dab4a74e7e8d33b011685f7c199
TG Frerich <themuppet.coder@gmail.com>
2023-03-29 12:23:55 +0200

    created isValidURL method in PodServcie to check if the sent URL by the user is a valid URL beginning with http:// Additionally sendUrl method is changed to return a RequestBodyForAssemblyAI Object instead of string

--- backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
+++ backend/src/main/java/com/github/tgfrerich/backend/service/PodService.java
@@ -1,19 +1,33 @@
 package com.github.tgfrerich.backend.service;
 
+import com.github.tgfrerich.backend.model.RequestBodyForAssemblyAI;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URL;
 
 @Service
 public class PodService {
 
 
-    public String sendUrl(String url) {
-        if (url == null || url.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    public RequestBodyForAssemblyAI sendUrl(String url) {
+        if (isValidURL(url)) {
+            RequestBodyForAssemblyAI requestBodyForAssemblyAI = new RequestBodyForAssemblyAI(url);
+            return requestBodyForAssemblyAI;
         } else {
-            return url;
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+    }
+
+    public boolean isValidURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            url.toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
