8185c921d6b6029128e66ac7c71ced68717d4c3e
TG Frerich <themuppet.coder@gmail.com>
2023-03-29 14:23:57 +0200

    added Lombok annotation for AssemblyAIApiResponse

--- backend/src/main/java/com/github/tgfrerich/backend/model/AssemblyAIApiResponse.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/AssemblyAIApiResponse.java
@@ -1,5 +1,10 @@
 package com.github.tgfrerich.backend.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AssemblyAIApiResponse {
 
     String id;
