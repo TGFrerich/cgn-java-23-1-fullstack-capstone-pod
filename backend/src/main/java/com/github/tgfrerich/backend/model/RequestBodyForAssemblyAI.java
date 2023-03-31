5b2e5288ad979f3bca41f8c3ac279986dc5e56e6
TG Frerich <themuppet.coder@gmail.com>
2023-03-31 12:37:02 +0200

    added id attribute for RequestBodyForAssemblyAI

--- backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
@@ -1,11 +1,12 @@
 package com.github.tgfrerich.backend.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class RequestBodyForAssemblyAI {
 
+    String id;
     String audio_url;
 }
