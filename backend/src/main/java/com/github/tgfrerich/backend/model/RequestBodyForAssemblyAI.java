aa6ead072c4392b6c7a53150d197ebe57a8ef314
TG Frerich <themuppet.coder@gmail.com>
2023-04-01 21:45:40 +0200

    adapted RequestBodyForAssemblyAI to request auto_chapter and speaker detection

--- backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
@@ -1,12 +1,16 @@
 package com.github.tgfrerich.backend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class RequestBodyForAssemblyAI {
 
     String id;
     String audio_url;
+    Boolean auto_chapters;
+    Boolean speaker_labels;
 }
