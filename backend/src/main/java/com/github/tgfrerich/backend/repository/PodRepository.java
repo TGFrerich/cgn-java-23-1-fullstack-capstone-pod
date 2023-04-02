12c87c1b193e8eaab2a65a78c0f727808e496f3d
TG Frerich <themuppet.coder@gmail.com>
2023-04-02 21:14:06 +0200

    annotated @Query so methods can search for variables with underscore ("_") in them

--- backend/src/main/java/com/github/tgfrerich/backend/repository/PodRepository.java
+++ backend/src/main/java/com/github/tgfrerich/backend/repository/PodRepository.java
@@ -3,6 +3,7 @@ package com.github.tgfrerich.backend.repository;
 
 import com.github.tgfrerich.backend.model.TranscribedPodcastFromAssemblyAI;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,10 +11,14 @@ import java.util.Optional;
 @Repository
 public interface PodRepository extends MongoRepository<TranscribedPodcastFromAssemblyAI, String> {
     boolean existsById(String id);
+
     Optional<TranscribedPodcastFromAssemblyAI> findById(String id);
 
-    boolean existsByAudioUrl(String audio_url);
-    Optional <TranscribedPodcastFromAssemblyAI> findByAudioUrl(String audio_url);
+    @Query("{'audio_url': ?0}")
+    boolean existsByAudio_Url(String audio_url);
+
+    @Query("{'audio_url': ?0}")
+    Optional<TranscribedPodcastFromAssemblyAI> findByAudioUrl(String audio_url);
 
 
 }
