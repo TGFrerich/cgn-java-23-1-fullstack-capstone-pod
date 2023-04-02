fc8ea208efdeed7da1bdba2d5d956e125f9ff045
TG Frerich <themuppet.coder@gmail.com>
2023-04-02 21:12:10 +0200

    adapted RequestBodyForAssemblyAI

--- backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/RequestBodyForAssemblyAI.java
@@ -1,5 +1,6 @@
 package com.github.tgfrerich.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestBodyForAssemblyAI {
+    @JsonProperty("audio_url")
+    private String audio_url;
+    @JsonProperty("webhook_url")
+    private String webhook_url;
+    @JsonProperty("auto_chapters")
+    private boolean auto_chapters;
 
-    String id;
-    String audio_url;
-    Boolean auto_chapters;
-    Boolean speaker_labels;
+    @JsonProperty("speaker_labels")
+    private boolean speaker_labels;
+
+
+    public RequestBodyForAssemblyAI(String audio_url, String webhook_url) {
+        this.audio_url = audio_url;
+        this.auto_chapters = true;
+        this.speaker_labels = true;
+        this.webhook_url = webhook_url;
+    }
 }
