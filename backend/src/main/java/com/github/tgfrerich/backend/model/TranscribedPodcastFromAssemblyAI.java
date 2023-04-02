be5c1263cb0818408f496042580a01f81e30b172
TG Frerich <themuppet.coder@gmail.com>
2023-04-02 21:11:45 +0200

    adapted TranscribedPodcastFromAssemblyAI

--- backend/src/main/java/com/github/tgfrerich/backend/model/TranscribedPodcastFromAssemblyAI.java
+++ backend/src/main/java/com/github/tgfrerich/backend/model/TranscribedPodcastFromAssemblyAI.java
@@ -2,24 +2,28 @@ package com.github.tgfrerich.backend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document("podcasts")
 public class TranscribedPodcastFromAssemblyAI {
     private String id;
-    private String acousticModel;
-    private double audioDuration;
-    private String audioUrl;
-    private String languageCode;
-    private double confidence;
-    private Boolean formatText;
-
-    private String languageModel;
-    private Boolean punctuate;
+    private String audio_url;
     private String status;
-    private String text;
+    private double audio_duration;
+    private String acoustic_model;
+    private String language_model;
+    private String language_code;
+    private Boolean format_text;
+    private Boolean punctuate;
+    private List<UtterancesFromSpeakersInTranscribedPodcast> utterances;
+    private Boolean auto_chapters;
+    private List<ChaptersInTranscribedPodcast> chapters;
 
 
 }
