9e5311920a671dd6f237aacbc68e970bbf56ead2
TG Frerich <themuppet.coder@gmail.com>
2023-04-11 16:19:24 +0200

    implemented useEffect in UseSendUrl hook

--- frontend/src/Hooks/UseSendUrl.tsx
+++ frontend/src/Hooks/UseSendUrl.tsx
@@ -1,32 +1,44 @@
-import {FormEvent, useState} from "react";
-import {TranscribedPodcastFromAssemblyAI} from "../Model/TransribedPodcastFromAssemblyAI";
-import axios from "axios";
+import {FormEvent, useEffect, useRef, useState} from 'react';
+import {TranscribedPodcastFromAssemblyAI} from '../Model/TransribedPodcastFromAssemblyAI';
+import axios from 'axios';
 
 export default function UseSendUrl() {
-
     const [podcast, setPodcast] = useState('');
     const [loading, setLoading] = useState(false);
     const [response, setResponse] = useState<TranscribedPodcastFromAssemblyAI | null>(null);
+    const isMounted = useRef(true);
+
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setLoading(true);
-        await axios.post('/api/podcasts', {
-            audio_url: podcast,
-            language_code: "",
-
-        })
+        await axios
+            .post('/api/podcasts', {
+                audio_url: podcast,
+                language_code: '',
+            })
             .then((result) => {
-                setPodcast('');
-                setResponse(result.data);
+                console.log('API response:', result); // Log the API response
+                if (isMounted.current) {
+                    setPodcast('');
+                    setResponse(result.data.data);
+                }
             })
             .catch((error) => {
-                console.log(error.response.data.error);
+                if (isMounted.current) {
+                    console.error('API error:', error); // Log the API error
+                }
             })
             .finally(() => {
-                setLoading(false);
+                if (isMounted.current) {
+                    setLoading(false);
+                }
             });
     };
-    return {handleSubmit, podcast, setPodcast, loading, setLoading, response}
-
+    return {handleSubmit, podcast, setPodcast, loading, setLoading, response};
 }
