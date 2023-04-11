a53473d2c24034eda33d728bc5b13615e4b77ca7
TG Frerich <themuppet.coder@gmail.com>
2023-04-11 16:03:27 +0200

    modified PodcastData.tsx to show the duration of the podcast as well as the the text that each speaker has spoken

--- frontend/src/Model/PodcastCard.tsx
+++ frontend/src/Model/PodcastCard.tsx
@@ -6,10 +6,22 @@ type PodcastCardProps = {
 };
 
 function PodcastCard({podcastData}: PodcastCardProps) {
-    // Display the podcast data in a card format
+    const formatDuration = (duration: number) => {
+        const hours = Math.floor(duration / 3600);
+        const minutes = Math.floor((duration % 3600) / 60);
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    };
+
     return (
         <div className="podcast-card">
-            {/* Display the podcast data here */}
+            <h3>Audio Duration: {formatDuration(podcastData.audio_duration)}</h3>
+            <h4>Utterances</h4>
+            {podcastData.utterances && podcastData.utterances.map((utterance, index) => (
+                <div key={index} style={{marginBottom: '1rem'}}>
+                    <strong>Speaker {utterance.speaker}:</strong>
+                    <p>{utterance.text}</p>
+                </div>
+            ))}
         </div>
     );
 }
